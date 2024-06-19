596b5889ec545210ed73ee7a0de7fc74686b587c
Chi Dan Nguyen - ADM <adm-sim8hu@Rsyd.net>
2024-06-19 17:26:57 +0200

    add VS_Projekt folder
    created the ASP.NET Core Web App (Model-View-Controller) project

---
+++ VS_Projekt/BirthdayReminder/BirthdayReminder/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using BirthdayReminder.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BirthdayReminder.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
