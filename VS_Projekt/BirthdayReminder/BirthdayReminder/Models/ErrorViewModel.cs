6832b173d2a6e1eebf2cd8bd66977c7556a1f304
Chi Dan Nguyen - ADM <adm-sim8hu@Rsyd.net>
2024-06-22 19:29:57 +0200

    Revert main branch to initial commit

---
+++ VS_Projekt/BirthdayReminder/BirthdayReminder/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BirthdayReminder.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
