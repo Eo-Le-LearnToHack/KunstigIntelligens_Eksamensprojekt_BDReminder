73448aa06e1cce1cc144a30942f2c91332e51b55
Chi Dan Nguyen - ADM <adm-sim8hu@Rsyd.net>
2024-06-26 18:52:31 +0200

    Create Database - Automatically created DB using IdentityUser: Denne klasse er en del af ASP.NET Core Identity og indeholder alle nødvendige egenskaber til at repræsentere en bruger, såsom UserName, Email, PasswordHash, osv.

---
+++ VS_Projekt/BirthdayReminder/BirthdayReminder/Data/RoleInitializer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace BirthdayReminder.Data
+{
+    public class RoleInitializer
+    {
+        public static async Task InitializeAsync(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            string adminEmail = "admin@example.com";
+            string password = "Admin123!";
+            if (await roleManager.FindByNameAsync("admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("admin"));
+            }
+            if (await userManager.FindByNameAsync(adminEmail) == null)
+            {
+                IdentityUser admin = new IdentityUser { Email = adminEmail, UserName = adminEmail };
+                IdentityResult result = await userManager.CreateAsync(admin, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "admin");
+                }
+            }
+        }
+    }
+}
