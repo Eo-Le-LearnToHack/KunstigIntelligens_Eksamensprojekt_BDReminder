2ec8e56d556b76109b6e43da1135014e9a9362e4
Chi Dan Nguyen - ADM <adm-sim8hu@Rsyd.net>
2024-06-26 18:50:57 +0200

    Create Database - Automatically created DB using IdentityUser: Denne klasse er en del af ASP.NET Core Identity og indeholder alle nødvendige egenskaber til at repræsentere en bruger, såsom UserName, Email, PasswordHash, osv.

--- VS_Projekt/BirthdayReminder/BirthdayReminder/Program.cs
+++ VS_Projekt/BirthdayReminder/BirthdayReminder/Program.cs
@@ -1,15 +1,30 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using BirthdayReminder.Data;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultUI(); // Add this line if you are using default UI
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -18,10 +33,22 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+
+// Initialize roles
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+    RoleInitializer.InitializeAsync(userManager, roleManager).Wait();
+}
 
 app.Run();
